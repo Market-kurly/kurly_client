afc7f4613e83077dd1239974d36c961a686d1516
leehyeonj <billaa@naver.com>
2021-07-17 18:52:58 +0900

    상세페이지 뷰

--- src/elements/Button.js
+++ src/elements/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from 'styled-components';
 
 const Button = (props)=>{
-    const {margin, padding,width, height, background_color, font_color, _onClick, border_radius, children} = props;
+    const {fontsize, border, margin, padding,width, height, background_color, font_color, _onClick, border_radius, children} = props;
     
     const styles = {
         width: width,
@@ -12,6 +12,8 @@ const Button = (props)=>{
         border_radius: border_radius,
         margin: margin,
         padding: padding,
+        border: border,
+        fontsize: fontsize
     }
     
     return (
@@ -34,10 +36,15 @@ Button.defaultProps = {
     children: null,
     margin: false,
     padding: false,
+    border:false,
+    fontsize: false,
 
 }
 
 const Btn = styled.button`
+ ${(props) => (props.border ? `border: ${props.border};` : "")}
+ ${(props) => (props.fontsize ? `font-size: ${props.fontsize};` : "")}
+    outline: 0;
     width: ${(props)=> (props.width)};
     height: ${(props)=> (props.height)};
     ${(props) => (props.background_color ? `background-color: ${props.background_color};` : "")}
--- src/shared/App.js
+++ src/shared/App.js
@@ -5,6 +5,7 @@ import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Detail from "../pages/Detail";
 import Header from "../components/Header";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <ConnectedRouter history={history}>
         <Header/>
         <Route path="/" exact ></Route>
+        <Route path="/detail" exact component={Detail}></Route>
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
       </ConnectedRouter>
