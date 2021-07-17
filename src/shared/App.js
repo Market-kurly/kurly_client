dfea0997e2752b5ab49245ffc516401a6abe15a6
leehyeonj <billaa@naver.com>
2021-07-17 16:50:52 +0900

    Header적용

--- src/shared/App.js
+++ src/shared/App.js
@@ -5,11 +5,13 @@ import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Header from "../components/Header";
 
 function App() {
   return (
     <React.Fragment>
       <ConnectedRouter history={history}>
+        <Header/>
         <Route path="/" exact ></Route>
         <Route path="/login" exact component={Login} />
         <Route path="/signup" exact component={Signup} />
