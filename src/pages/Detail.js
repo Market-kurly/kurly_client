259818d10bc5a9c6f0fa38d855bf7b3e669847a7
leehyeonj <billaa@naver.com>
2021-07-19 15:40:54 +0900

    상세페이지에서 구매 수량 측정하는 컴포넌트 생성

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,33 +2,22 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { actionCreators as productActions } from "../redux/modules/product";
 import {Grid, Image, Text, Button} from "../elements"
+import CountNum from "../components/CountNum";
 
 const Detail = (props) =>{
     // const dispatch = useDispatch();
     // const product_id = props.match.params.id;
     // const product = useSelector(state=> state.product.product);
 
-
-
+    const [purchaseNum, setPurchaseNum] = React.useState(1);
+ 
     // React.useEffect(()=>{
     //     dispatch(productActions.getOneProductSV(product_id));
     // },[]);
 
 
     const {productImg, productName, price, amount, unit, delivery, packing, expiryDate, country, infromation, keeping} = props;
-    // {
-//     "productImg":"imgUrl",
-//     "productName":"상품 이름",
-//     "price":"22000",
-//     "amount" : "250g",
-//     "unit" : "1봉",
-//     "delivery" : "새벽배송",
-//     "packing" : "상온/종이포장",
-//     "expiryDate": "수령일 포함 최소 125일",
-//     "country":"원산지",
-//     "information":"안내사항",
-//     "keeping":"냉동상품"
-//     }
+    let totalPrice = price * purchaseNum;
     return (
         <React.Fragment>
             <Grid display="flex" width="1050px" padding="20px 0px" margin="0 auto">
@@ -40,7 +29,7 @@ const Detail = (props) =>{
                         <Text margin="4px 60px 0px 0px" color="#999" padding="4px 60px 0 0">{infromation}</Text>
                     </Grid>
                     <Grid>
-                        <Text margin="0px" size="28px">{price}</Text>
+                        <Text margin="0px" size="28px">{price}원</Text>
                         <Text margin="0px"padding="7px 0px "color="#5f0080">로그인후, 적립혜택이 제공됩니다.</Text>
                     </Grid>
                     <Grid>
@@ -70,11 +59,13 @@ const Detail = (props) =>{
                         </Grid>
                         <Grid display="flex" padding="18px 0px">
                             <Text width="150px"margin="0px"color="#666">구매수량</Text>
-                            <Text margin="0px">숫자올라가는것</Text>
+                            <CountNum num={purchaseNum} setNum={setPurchaseNum}></CountNum>
+                          
+                            
                         </Grid>
                         <Grid display="flex" justify="flex-end">
-                            <Text lineHeight="60px" margin="0px" size="13px">총상품금액:</Text>
-                            <Text margin="0px" size="32px" weight="800">16500원</Text>
+                            <Text lineHeight="60px" margin="0px 10px" size="13px">총상품금액:</Text>
+                            <Text margin="0px" size="32px" weight="800">{totalPrice} 원</Text>
                           
                         </Grid>
                         <Grid display="flex">
@@ -107,7 +98,7 @@ const Detail = (props) =>{
 Detail.defaultProps = {
     productImg : "default",
     productName : "default",
-    price : "default",
+    price : "2000",
     amount : "default",
     unit: "default",
     delivery : "default",
